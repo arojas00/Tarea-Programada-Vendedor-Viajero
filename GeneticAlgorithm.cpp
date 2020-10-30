eaf062cde6d22206aa4049a29c570360e8138f1a
arojas00 <55304806+arojas00@users.noreply.github.com>
2020-10-29 19:10:03 -0600

    Add files via upload

---
+++ GeneticAlgorithm.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "GeneticAlgorithm.h"
+
+GeneticAlgorithm::GeneticAlgorithm(int populationSize,float elitismRatio,float mutationRatio,float sporadicRatio){
+    GeneticAlgorithm::populationSize = populationSize;
+    GeneticAlgorithm::elitismRatio = elitismRatio;
+    GeneticAlgorithm::mutationRatio = mutationRatio;
+    GeneticAlgorithm::sporadicRatio = sporadicRatio;
+}
+GeneticAlgorithm::~GeneticAlgorithm(){
+}
+unsigned int GeneticAlgorithm::getEpoch(){
+    return currentEpoch;
+}
+void GeneticAlgorithm::reset(int populationSize,float elitismRatio,float mutationRatio,float sporadicRatio){
+    GeneticAlgorithm::currentEpoch = 1;
+    GeneticAlgorithm::populationSize = populationSize;
+    GeneticAlgorithm::elitismRatio = elitismRatio;
+    GeneticAlgorithm::mutationRatio = mutationRatio;
+    GeneticAlgorithm::sporadicRatio = sporadicRatio;
+}
+
+main(){
+    char c;
+    std::cin >> c;
+    return 0;
+}
