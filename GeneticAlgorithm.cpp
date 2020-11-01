7847cf76663315352745bf18abdd065116fd29da
arojas00 <55304806+arojas00@users.noreply.github.com>
2020-10-31 19:47:11 -0600

    Add files via upload

--- GeneticAlgorithm.cpp
+++ GeneticAlgorithm.cpp
@@ -21,10 +21,31 @@ void GeneticAlgorithm::reset(int populationSize,float elitismRatio,float mutatio
 }
 
 class TravellingSalesman : public GeneticAlgorithm{
-    
+    public:
+        TravellingSalesman(int populationSize,float elitismRatio,float mutationRatio,float sporadicRatio) : GeneticAlgorithm(populationSize, elitismRatio, mutationRatio, sporadicRatio){
+        }
+        ~TravellingSalesman(){
+        }
+        void reset(int populationSize,float elitismRatio,float mutationRatio,float sporadicRatio){
+            GeneticAlgorithm::reset(populationSize, elitismRatio, mutationRatio, sporadicRatio);
+        }
+    	void epoch(){
+            
+        }
+        float getBestIndividual(){
+            
+        }
+    	void drawBestIndividual(float** points,unsigned int &numPoints){
+            
+        }
+        void selection(){}
+        void crossover(){}
+        void mutation(){}
+        void fitness(){}
 };
 
 main(){
+    TravellingSalesman* travellingSalesman = new TravellingSalesman(1,2.0f,3.0f,4.0f);
     char c;
     std::cin >> c;
     return 0;
